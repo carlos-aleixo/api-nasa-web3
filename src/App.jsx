77053b5ae7c11d189d27890848183d8f30630a33
carloxo307 <carlos.aleixo@rede.ulbra.br>
2023-11-29 17:33:06 -0300

    mudanças estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ReactPaginate from 'react-paginate';
+import './App.scss';
 
 const App = () => {
   const [neoData, setNeoData] = useState([]);
